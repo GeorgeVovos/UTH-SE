4c8f5f28f1bc2ebfff3e50b2e745700ffc538b06
GeorgeVovos <George.Vovos@prometric.com>
2025-01-18 15:29:41 +0000

    Added class diagrams

--- Exercises/04-Recipes/Uth.Recipes.Domain/RecipeExecution/StepBasedRecipeExecution.cs
+++ Exercises/04-Recipes/Uth.Recipes.Domain/RecipeExecution/StepBasedRecipeExecution.cs
@@ -4,7 +4,7 @@ namespace Uth.Recipes.Domain.RecipeExecution
 {
     public class StepBasedRecipeExecution : BaseRecipeExecution
     {
-        public StepBasedRecipeExecution(List<IRunnableOperation> steps) : base(steps)
+        StepBasedRecipeExecution() : base(steps)
         {
         }
         protected override double GetCurrentProgress() => (double)(CurrentStepIndex - 1) / Steps.Count * 100;
